777576e2ebb09e3d8f2d72bc70ff447d57a87f95
GAMBLIN Vincent <yuumie31@gmail.com>
2024-09-18 09:16:12 +0200

    ajout de bootstrap

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,20 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/company', name: 'company_index')]
+#[Route('/company')]
 class CompanyController extends AbstractController
 {
     #[Route('/', name: 'company_index')]
-    public function index(): Response
+    public function index(CompanyRepository $companyRepository, Request $request): Response
     {
+        $companies = $companyRepository->findAll();
+        dump($request);
+
+
+
         return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
+            'companies' => $companies,
         ]);
     }
-
+    
     #[Route('/add1', name: 'company_add1')]
-    public function addFirst(): Response
+    public function addFirst(EntityManagerInterface $entityManager): Response
     {
-        return new Response("Entreprise 'STURNO' Crée avec succès ! ");
+        $sturno = new Company();
+        $sturno->setName("STURNO");
+        // $entityManager = $this-getDoctrine()->getManager();
+        $entityManager->persist($sturno);
+        $entityManager->flush();
+        return new Response("Entreprise 'STURNIO' créee avec succès !")
+        ;
     }
 }
